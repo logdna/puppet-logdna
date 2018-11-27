9641198e7bd085e56121c41aadd641935e85f1f2
Samir Musali <samir.musali@logdna.com>
2018-11-27 17:11:10 -0500

    Touching Specs v2

--- spec/classes/logdna_spec.rb
+++ spec/classes/logdna_spec.rb
@@ -1,21 +1,53 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe 'logdna' do
-  on_supported_os.each do |os, facts|
-    context 'with default values for all parameters' do
+describe "logdna" do
+  context "unsupported operating system" do
+    describe "logdna class without any parameters on Solaris/Nexenta" do
       let(:facts) do
-        facts
+        {
+          osfamily:         "Solaris",
+          operatingsystem:  "Nexenta"
+        }
       end
-      case facts[:osfamily]
-      when ('Debian' or 'RedHat')
-        it {
-          is_expected.to compile.with_all_deps
-          should contain_class('logdna')
-        }
-      else
-        it {
-          is_expected.not_to compile.with_all_deps.and_raise_error(Puppet::ParseError)
-        }
+
+      it do
+        is_expected.not_to compile.with_all_deps.and_raise_error(Puppet::ParseError)
+      end
+    end
+  end
+
+  # Test all supported OSes
+  context "all supported operating systems" do
+    on_supported_os.each do |os, facts|
+      describe "logdna class common actions on #{os}" do
+        let(:facts) do
+          facts
+        end
+
+        it { should compile.with_all_deps }
+
+        it { should contain_class("logdna") }
+
+        describe "logdna imports the default params" do
+          it { should contain_class("logdna::params") }
+        end
+
+        it { should contain_file("/etc/logdna.conf") }
+
+        it { should contain_class("logdna::agent::service") }
+
+        it { should contain_class("logdna::agent::configure") }
+
+        case facts[:osfamily]
+        when 'Debian'
+          it { should contain_class("logdna::agent::package::install_debian") }
+        when 'RedHat'
+          it { should contain_class("logdna::agent::package::install_redhat") }
+        else
+          it {
+            is_expected.not_to compile.with_all_deps.and_raise_error(Puppet::ParseError)
+          }
+        end
       end
     end
   end
